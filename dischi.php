8a4f105a791c7194bfc7014db757b12d70e1e9e5
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-07-28 16:09:59 +0200

    init

---
+++ dischi.php
@@ -0,0 +1,62 @@
+<?php 
+    
+    $dischi =[
+
+        [
+            'title' => 'The Dark Side Of The Moon',
+            'author' => 'Pink Floyd',
+            'year' => 1973,
+            'poster' => 'https://static.billboard.com/files/media/Pink-Floyd-Dark-Side-of-the-Moon-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => 'My Beautiful Dark Twisted Fantasy',
+            'author' => 'Kanye West',
+            'year' => 2010,
+            'poster' => 'https://static.billboard.com/files/media/Kanye-West-My-Beautiful-Dark-Twisted-Fantasy-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => "If You're Reading This It's Too Late",
+            'author' => 'Drake',
+            'year' => 2015,
+            'poster' => 'https://static.billboard.com/files/media/If-youre-reading-this-its-too-late-drake-no1-albums-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => 'Illmatic',
+            'author' => 'Nas',
+            'year' => 1994,
+            'poster' => 'https://static.billboard.com/files/media/Nas-Illmatic-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => 'To Pimp a Butterfly',
+            'author' => 'Kendrick Lamar',
+            'year' => 2015,
+            'poster' => 'https://static.billboard.com/files/media/To-pimp-a-butterfly-kendrick-lamar-no1-albums-billboard-1000x1000-compressed.jpghttps://static.billboard.com/files/media/To-pimp-a-butterfly-kendrick-lamar-no1-albums-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => "Sgt. Pepper's Lonely Hearts Club Band",
+            'author' => 'The Beatles',
+            'year' => 1967,
+            'poster' => 'https://static.billboard.com/files/media/The-Beatles-Sgt-Peppers-lonely-hearts-club-band-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => 'Elvis Presley',
+            'author' => 'Elvis Presley',
+            'year' => 1956,
+            'poster' => 'https://static.billboard.com/files/media/Elvis-Presley-Elvis-Presley-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => 'Nevermind',
+            'author' => 'Nirvana',
+            'year' => 1991,
+            'poster' => 'https://static.billboard.com/files/media/Nirvana-Nevermind-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+        [
+            'title' => 'Ready to Die',
+            'author' => 'The Notorious B.I.G.',
+            'year' => 1994,
+            'poster' => 'https://static.billboard.com/files/media/The-Notorious-B-I-G-Ready-to-Die-album-covers-billboard-1000x1000-compressed.jpg'
+        ],
+
+    ]
+    
+?>
---
+++ index-php.php
@@ -0,0 +1,26 @@
+<?php
+
+    include 'dischi.php';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Ajax Dischi</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row row-cols-4">
+            <?php  ?>
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+</html>
